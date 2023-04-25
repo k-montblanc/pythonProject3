c54d2b2ff2e7997778c0a5df3f9b6ff4e083aebe
k-montblanc <kylemontblanc@gmail.com>
2023-04-25 14:19:52 -0600

    Test

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+
+PIXELA_ENDPOINT = 'https://pixe.la/v1/users'
+
+
+user_params = {
+    'token': 'leivnlikldvlkvndvinosivn',
+    'username': "kmontblanc",
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes'
+}
+#create username
+# response = requests.post(url=PIXELA_ENDPOINT,json=user_params)
+# print(response.text)
